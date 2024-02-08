1e9368fef9cd3557196136c52dcc9ddf0726ea4b
Carson Cheng <carsoncheng@Admins-MacBook-Air.local>
2024-02-08 12:31:23 +0800

    Initial commit

---
+++ life.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "life.h"
+// test script for life.h library
+int main(){
+    std::cout << decompress("3o2b3o!") << std::endl;
+    std::vector< std::vector<int> > empty;
+    std::vector< std::vector<int> > res = paste(empty, decompress("3o2b3o$o!"), 1, 1);
+    res = paste(res, decompress("3o2b3o$o!"), 1, 4);
+    for(int i=0; i<res.size(); i++){
+        std::cout << res[i][0] << ", " << res[i][1] << std::endl;
+    }
+    std::vector<int> box = getbbox(res);
+    std::cout << box[0] << ", " << box[1] << ", " << box[2] << ", " << box[3] << std::endl;
+    std::string rle = getrle(res);
+    std::cout << rle << std::endl;
+    for(int i=0; i<1000; i++){
+        //std::vector< std::vector<int> > pat = {{18, 13}, {19, 14}, {17, 15}, {18, 15}, {19, 15}, {53, 15}, {52, 16}, {52, 17}, {53, 17}, {54, 17}, {22, 39}, {23, 39}, {24, 39}, {24, 40}, {23, 41}};
+        std::vector< std::vector<int> > pat = {{29, 29}, {29, 30}, {30, 28}, {30, 31}, {31, 29}, {31, 30}, {31, 31}};
+        std::vector< std::vector<int> > newpat = run(pat, 100);
+        std::vector<int> box = getbbox(newpat);
+        //std::cout << box[0] << ", " << box[1] << std::endl;
+        int pop = getpop(newpat);
+        //std::cout << pop << std::endl;
+    }
+    //right now it does about 1000 iterations per second, which is way too slow but can be improved.
+}
---
+++ life.h
@@ -0,0 +1,143 @@
+// a simple b3s23 iterator based on cell lists
+// used for preprocessing patterns before piping into more complicated programs like apgsearch
+#include <iostream>
+#include <string>
+#include <vector>
+std::string decompress(std::string rle){
+    std::string res;
+    int buffer = 0;
+    for (int i=0; i<rle.size(); i++){
+        //std::cout << rle[i] << std::endl;
+        if (isdigit(rle[i])){
+            if (buffer == 0){
+                int k = rle[i] - 48; // for some reason we need to subtract 48 (by empirical testing)
+                buffer = k;
+                //std::cout << buffer << std::endl;
+            } else {
+                int k = rle[i] - 48; // same here, probably
+                buffer = 10 * buffer + k;
+            }
+        } else {
+            if (buffer != 0){
+                for (int j=0; j<buffer; j++){
+                    res += rle[i];
+                }
+            } else {
+                res += rle[i];
+            }
+            buffer = 0;
+        }
+    }
+    return res;
+}
+std::vector< std::vector<int> > paste(std::vector< std::vector<int> > res, std::string rle, int x, int y){
+    // generates a cell list based on decompressed rle
+    int x_original = x;
+    std::string dollar = "$";
+    std::string exc = "!";
+    std::string on = "o";
+    for (int i=0; i<rle.size(); i++){
+        std::string cur(1, rle[i]);
+        if (cur == dollar){
+            y += 1;
+            x = x_original;
+        } else if (cur != exc){
+            if (cur == on){
+                res.push_back({x, y});
+            }
+            x += 1;
+        }
+    }
+    return res;
+}
+std::vector< std::vector<int> > run(std::vector< std::vector<int> > pat, int gens){
+    //this iterator is very simple, and does not have any optimization over escaping spaceships
+    //step_size must be a divisor of gens, or the program might not work properly.
+    //limits pattern size to at most 64x64
+    //works weirdly when values of the cell list gets close to zero
+    for(int i=0; i<gens; i++){
+        int xmin=2147483647; int ymin=2147483647; int xmax=0; int ymax=0;
+        bool cells[64][64] = {{false}};
+        for(int i=0; i<pat.size(); i++){
+            cells[pat[i][0]][pat[i][1]] = true;
+            if (xmin > pat[i][0]){
+                xmin = pat[i][0];
+            } else if (xmax < pat[i][0]){
+                xmax = pat[i][0];
+            }
+            if (ymin > pat[i][1]){
+                ymin = pat[i][1];
+            } else if (ymax < pat[i][1]){
+                ymax = pat[i][1];
+            }
+        }
+        //run the pattern for 1 generation
+        bool newcells[64][64] = {{false}};
+        std::vector< std::vector<int> > newpat;
+        for(int i=xmin-1; i<xmax+2; i++){
+            for(int j=ymin-1; j<ymax+2; j++){
+                std::vector<bool> neighbors;
+                neighbors = {cells[i-1][j-1], cells[i][j-1], cells[i+1][j-1], cells[i-1][j], cells[i+1][j], cells[i-1][j+1], cells[i][j+1], cells[i+1][j+1]};
+                if (cells[i][j] == false){
+                    if (std::count(neighbors.begin(), neighbors.end(), true) == 3){
+                        newcells[i][j] = true;
+                        newpat.push_back({i, j});
+                    }
+                } else {
+                    int cnt = std::count(neighbors.begin(), neighbors.end(), true);
+                    if (cnt == 2 or cnt == 3){
+                        newcells[i][j] = true;
+                        newpat.push_back({i, j});
+                    }
+                }
+            }
+        }
+        if (pat == newpat){
+            return pat;
+        }
+        pat = newpat;
+        if (pat.size() == 0){
+            return pat;
+        }
+    }
+    return pat;
+}
+int getpop(std::vector< std::vector<int> > pat){
+    return pat.size();
+}
+std::vector<int> getbbox(std::vector< std::vector<int> > pat){
+    int xmin=2147483647; int ymin=2147483647; int xmax=0; int ymax=0;
+    for(int i=0; i<pat.size(); i++){
+        if (xmin > pat[i][0]){
+            xmin = pat[i][0];
+        } else if (xmax < pat[i][0]){
+            xmax = pat[i][0];
+        }
+        if (ymin > pat[i][1]){
+            ymin = pat[i][1];
+        } else if (ymax < pat[i][1]){
+            ymax = pat[i][1];
+        }
+    }
+    return {xmin, ymin, xmax - xmin + 1, ymax - ymin + 1};
+}
+std::string getrle(std::vector< std::vector<int> > pat){
+    // TODO: sort the celllist vector so that it becomes less expensive to generate the rle
+    std::vector<int> bbox = getbbox(pat);
+    std::string rle = "x = 0, y = 0, rule = B3/S23\n";
+    for (int y=bbox[1]; y<(bbox[1]+bbox[3]); y++){
+        for (int x=bbox[0]; x<(bbox[0]+bbox[2]); x++){
+            std::vector<int> val = {x, y};
+            if(std::find(pat.begin(), pat.end(), val)!=pat.end()){
+                // Found the item
+                rle += "o";
+                std::cout << x << ", " << y << std::endl;
+            } else {
+                rle += "b";
+            }
+        }
+        rle += "$";
+    }
+    rle += "!";
+    return rle;
+}
